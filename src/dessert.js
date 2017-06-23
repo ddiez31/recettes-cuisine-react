713c25ce756d5a2905a77de276b32006a84ef58d
David <david.diez31@free.fr>
2017-06-23 16:57:31 +0200

    first commit

---
+++ src/dessert.js
@@ -0,0 +1,17 @@
+module.exports = {
+    dessert1: {
+        titreDessert: 'titre1 test',
+        imageDessert: 'image1 test',
+        detailDessert: 'detail1 test',
+    },
+    dessert2: {
+        titreDessert: 'titre2 test',
+        imageDessert: 'image2 test',
+        detailDessert: 'detail2 test',
+    },
+    dessert3: {
+        titreDessert: 'titre3 test',
+        imageDessert: 'image3 test',
+        detailDessert: 'detail3 test',
+    },
+}
---
+++ src/entree.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entree1: {
+        titreEntree: 'titre1 test',
+        imageEntree: 'image1 test',
+        detailEntree: 'detail1 test',
+    },
+    entree2: {
+        titreEntree: 'titre2 test',
+        imageEntree: 'image2 test',
+        detailEntree: 'detail2 test',
+    },
+    entree3: {
+        titreEntree: 'titre3 test',
+        imageEntree: 'image3 test',
+        detailEntree: 'detail3 test',
+    },
+}
